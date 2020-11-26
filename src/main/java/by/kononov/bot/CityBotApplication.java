de6ac23d19652b2c3fa5d3f2c5fb614a55ee6017
Vitaly Kononov <vitalij_kononov@epam.com>
2020-11-26 21:17:44 +0300

    branch tets

--- src/main/java/by/kononov/bot/CityBotApplication.java
+++ src/main/java/by/kononov/bot/CityBotApplication.java
@@ -2,7 +2,7 @@ package by.kononov.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// test
 @SpringBootApplication
 public class CityBotApplication {
 
